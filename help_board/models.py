0338744f71a8ba4996932db777a3959fd2cf0707
Sugandhi13 <>
2024-01-20 17:41:04 +0000

    Added Meta Class and methods in Query and Answer models

--- help_board/models.py
+++ help_board/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Query(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,6 +18,12 @@ class Query(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Answer(models.Model):
     query = models.ForeignKey(
@@ -27,3 +35,9 @@ class Answer(models.Model):
     query_answer = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Answer {self.query_answer} by {self.author}"
