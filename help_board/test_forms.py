6c76529a12fa1efd23b1aadc96ab841b286ed46a
Sugandhi . <sugandhisharma1988@gmail.com>
2024-02-18 04:04:39 +0000

    Added test case for query form and rename username in userprofile model

--- help_board/test_forms.py
+++ help_board/test_forms.py
@@ -1,13 +1,69 @@
 from django.test import TestCase
-from .forms import AnswerForm
+from .forms import QueryForm, AnswerForm
 
 
-class Testanswer_form(TestCase):
+class Testanswerform(TestCase):
+
 
     def test_form_is_valid(self):
-        answer_form = AnswerForm({'query_answer': 'This is a great post'})
-        self.assertTrue(answer_form.is_valid(), msg='Form is not valid')
+        answer_form = AnswerForm({'content': 'This is a great post'})
+        self.assertTrue(answer_form.is_valid(), msg_prefix='Form is valid')
 
     def test_form_is_invalid(self):
-        answer_form = AnswerForm({'query_answer': ''})
-        self.assertFalse(answer_form.is_valid(), msg='Form is valid')
+        answer_form = AnswerForm({'content': ''})
+        self.assertFalse(answer_form.is_valid(), msg_prefix='Form is not valid')
+
+
+class Testquery_form(TestCase):
+
+    def test_form_is_valid(self):
+        query_form = QueryForm(
+            {
+                'title': 'This is a title', 
+                'category': 'Travel',
+                'content': 'This is a great post'
+            }
+        )
+        self.assertTrue(query_form.is_valid(), msg='Query form is valid')
+
+    def test_form_have_invalid_name(self):
+        query_form = QueryForm(
+            {
+                'title': '', 
+                'category': 'Travel',
+                'content': 'This is a great post'
+            }
+        )
+        self.assertFalse(query_form.is_valid(), msg='Query title can not be empty, but the Form is valid')
+
+    def test_form_have_invalid_category(self):
+        query_form = QueryForm(
+            {
+                'title': 'This is a title', 
+                'category': '',
+                'content': 'This is a great post'
+            }
+        )
+        self.assertFalse(query_form.is_valid(), msg='Query category can not be empty, but the Form is valid')
+
+    def test_form_have_invalid_category_entered(self):
+        query_form = QueryForm(
+            {
+                'title': 'This is a title', 
+                'category': 'Random',
+                'content': 'This is a great post'
+            }
+        )
+        self.assertFalse(query_form.is_valid(), msg='Random category is invalid, but the Form is valid')
+
+    def test_form_have_invalid_content(self):
+        query_form = QueryForm(
+            {
+                'title': 'This is a title', 
+                'category': 'Travel',
+                'content': ''
+            }
+        )
+        self.assertFalse(query_form.is_valid(), msg='Query Content can not be empty, but the Form is valid')
+
+
